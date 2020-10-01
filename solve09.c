7a29704ca735d7d51e2a8d3c6b0c5e6b5006b805
Disum <54613404+Disum@users.noreply.github.com>
2020-10-01 23:00:32 +0300

    Minor changes

--- solve09.c
+++ solve09.c
@@ -8,14 +8,14 @@ int solve09(double (*f)(double), double a, double b, double eps, double *r)
 	double h = b - a, simp1, simp2, fa = f(a), fb = f(b), sum1, sum2;
 
 	sum1 = 0;
-	simp1 = h*fa/3 + h*f(a + h)*((double)4)/3 + h*fb/3;
+	simp1 = h*fa/3 + 4*h*f(a + h)/3 + h*fb/3;
 	h /= 2;
 	for( n = 1; n<N; n *= 2 )
 	{
 		for( i = 0, sum2 = 0; i<n; i++ )
 			sum2 += f(a + 2*i*h + h);
 		sum2 *= h;
-		simp2 = h*fa/3 + sum1*((double)2)/3 + sum2*((double)4)/3 + h*fb/3;
+		simp2 = h*fa/3 + 2*sum1/3 + 4*sum2/3 + h*fb/3;
 
 		if( fabs(simp2 - simp1)<eps )
 		{
